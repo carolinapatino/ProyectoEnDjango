6ccb800aedb2e63bb285484ce13beb5a817cc409
David Javier Monroy <davmonroyp@MacBook-Air.local>
2021-01-29 18:57:15 -0400

    admin superuser

--- pizzaucab/order/admin.py
+++ pizzaucab/order/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Size, Ingredient, Pizza, Order
 
-# Register your models here.
+admin.site.register(Size)
+admin.site.register(Ingredient)
+admin.site.register(Pizza)
+admin.site.register(Order)
