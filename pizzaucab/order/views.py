bdc1f9c0fd981ca787d9dcff7d2554070fd1a400
David Javier Monroy <davmonroyp@MacBook-Air.local>
2021-02-01 18:42:50 -0400

    interfaz principal (order)

--- pizzaucab/order/views.py
+++ pizzaucab/order/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Order, Size, Pizza, Ingredient
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Bienvenido a Pizzeria UCAB')
+    ingredients = Ingredient.objects.all()
+    sizes = Size.objects.all()
+    context = {
+        'ingredients': ingredients,
+        'sizes': sizes,       
+    }
+    return render(request,'order/index.html', context)
+
+def detail(request, order_id):
+    return HttpResponse("Esta buscando la pizza %s" % order_id)
+
